d3e63fb022839ab275cfec4472b65e590bf7c7e8
antonina.solodskaya <antonina.solodskaya@devit-team.com>
2020-04-08 01:11:11 +0300

    login user and reset password with send mail for user,add permissions

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,15 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
-const cors = require('cors');
 
 const server = createServer();
 
 server.express.use(cookieParser());
 
+// decode the JWT so we can get the user Id on each request
 server.express.use((req, res, next) => {
     const { token } = req.cookies;
     if (token) {
@@ -19,19 +20,27 @@ server.express.use((req, res, next) => {
     next();
 });
 
-// TODO Use express middlware to populate current user
-const corsOptions = {
-    origin: process.env.FRONTEND_URL,
-    credentials: true
-};
+// Create a middleware that populates the user on each request
 
-server.use(cors(corsOptions));
+server.express.use(async (req, res, next) => {
+    // if they aren't logged in, skip this
+    if (!req.userId) return next();
+    const user = await db.query.user(
+        { where: { id: req.userId } },
+        '{ id, permissions, email, name }'
+    );
+    req.user = user;
+    next();
+});
 
 server.start(
     {
-        cors: corsOptions
+        cors: {
+            credentials: true,
+            origin: process.env.FRONTEND_URL,
+        },
     },
     deets => {
-        console.log(`Server is now running on port http:/localhost:${deets.port}`);
+        console.log(`Server is now running on port http://localhost:${deets.port}`);
     }
-)
+);
