399c0f85cbeb8c8d521ea3b624afc22ebd10851d
antonina.solodskaya <antonina.solodskaya@devit-team.com>
2020-04-09 23:49:25 +0300

    prod url next

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -6,7 +6,7 @@ import { LOCAL_STATE_QUERY } from '../components/Cart';
 function createClient({ headers }) {
     return new ApolloClient({
         //add prodEndpoint
-        uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
+        uri: process.env.NODE_ENV === 'development' ? prodEndpoint : prodEndpoint,
         request: operation => {
             operation.setContext({
                 fetchOptions: {
